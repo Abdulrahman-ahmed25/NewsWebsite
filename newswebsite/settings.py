6a9d0a3fba4c2cd379869eb5b5b2cefe77f96588
Abdulrahman Ahmed <76537136+Abdulrahman-ahmed25@users.noreply.github.com>
2021-02-19 23:50:59 +0200

    Update settings.py

--- newswebsite/settings.py
+++ newswebsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h)r2bzwzj77u-k#9pe3i&^i^u#$)vo2@7xqjy&_u)k==p649s&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
